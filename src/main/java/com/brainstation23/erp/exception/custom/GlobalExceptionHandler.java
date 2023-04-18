6f71c7c01f87deca10f7ce0ff243e51f8b0677bb
WasifJavaBS1357 <wasif.islam@Brainstation-23.com>
2023-04-19 02:22:59 +0600

    Handle ExceptionGlobally

--- src/main/java/com/brainstation23/erp/exception/custom/GlobalExceptionHandler.java
+++ src/main/java/com/brainstation23/erp/exception/custom/GlobalExceptionHandler.java
@@ -1,10 +1,23 @@
 package com.brainstation23.erp.exception.custom;
 
-import lombok.extern.slf4j.Slf4j;
+import com.brainstation23.erp.exception.custom.custom.AlreadyExistsException;
+import com.brainstation23.erp.exception.custom.custom.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-@Slf4j
 public class GlobalExceptionHandler {
 	// TODO: handle exceptions globally
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFound(NotFoundException notFoundException){
+        return new ResponseEntity<String>(notFoundException.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    public ResponseEntity<String> handleAlreadyExistsException(AlreadyExistsException alreadyExistsException){
+        return new ResponseEntity<String>(alreadyExistsException.getMessage(), HttpStatus.NOT_FOUND);
+    }
 }
--- src/main/java/com/brainstation23/erp/exception/custom/custom/NotFoundException.java
+++ src/main/java/com/brainstation23/erp/exception/custom/custom/NotFoundException.java
@@ -1,7 +1,12 @@
 package com.brainstation23.erp.exception.custom.custom;
 
+import com.brainstation23.erp.exception.custom.custom.AlreadyExistsException;
+import com.brainstation23.erp.exception.custom.custom.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
